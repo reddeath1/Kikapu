4fb66f485b91e4653043da357b419ce07f2b450b
Reddeath <frankslayer1@gmail.com>
2024-03-26 14:39:28 +0300

    ready for production

--- src/views/account/components/UserAvatar.jsx
+++ src/views/account/components/UserAvatar.jsx
@@ -63,6 +63,7 @@ const UserNav = () => {
         />
       </div>
       <DownOutlined style={{ fontSize: '1.2rem', marginLeft: '1rem' }} />
+
       <div className="user-nav-sub">
         {profile.role !== 'ADMIN' && (
           <Link
